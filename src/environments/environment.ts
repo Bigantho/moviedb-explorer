66abc63fce428ec8a940a32914b0be244752289a
Bigantho <biganthdev@gmail.com>
2025-06-27 21:52:39 -0600

    removiendo los enviroments

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false,
-  tmdbApiKey: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhN2I1NWVjYzRiZjc1M2VmZjdjYjhhNWY2ZDhkMzk2MyIsIm5iZiI6MTc1MDM4NjM5NC4xODEsInN1YiI6IjY4NTRjNmRhNDg0ZjFjN2U2Zjg0ZTMwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QzyJWm4bzMaEb5sVv3P7h0ZyDffsPdqZOi9Jtl_oLN0', // Reemplaza con tu clave API o Access Token de TMDb
-  tmdbApiUrl: 'https://api.themoviedb.org/3'
-};
